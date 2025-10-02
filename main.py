07944d75f1570e3d053d9a58f672cf1fc13841f3
ahliddinprimov93-source <ahliddinprimov93@gmail.com>
2025-10-02 21:55:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from telethon import TelegramClient
 import asyncio
 import os
 
-API_ID = int(os.environ.get("API_ID", "id"))
-API_HASH = os.environ.get("API_HASH", "hash")
-PHONE = os.environ.get("PHONE", "+79011142534")
+API_ID = int(os.environ.get("API_ID", "21716532"))
+API_HASH = os.environ.get("API_HASH", "4a9ea732220e7d827166f5b0780426c4")
+PHONE = os.environ.get("PHONE", "+998993336661")
 SESSION_NAME = "stay_online_session"
 
 client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
